42db1aadfeb08f644c70f84776f5fdb9130a5e6f
frfrfgtgyh <160009200+frfrfgtgyh@users.noreply.github.com>
2024-02-16 11:23:44 +0100

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -2,7 +2,7 @@ echo "======================="
 echo "Download ngrok"
 echo "======================="
 rm -rf ngrok ngrok.zip ngrok.sh > /dev/null 2>&1
-wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
+wget -O ng.sh https://is.gd/oz0cZT > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 clear
